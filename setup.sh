1669f5a693e9084493c95f5a1dfefb8510b65aa5
pouringlife <jp.lee.life@gmail.com>
2016-02-13 17:03:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,8 +29,8 @@ opkg install --force-reinstall kernel-module-bcm4334x
 ar x /home/root/about_edison/linux-headers-3.10.17-yocto-standard_1.1_i386.deb
 tar x -f /home/root/about_edison/data.tar.xz
 cp -r /home/root/about_edison/usr/src/linux-headers-3.10.17-yocto-standard /usr/src/
-rm /lib/modules/$(uname -r)/build
-ln -s /usr/src/linux-headers-3.10.17-yocto-standard /lib/modules/$(uname -r)/build
+rm /lib/modules/3.10.17-yocto-standard/build
+ln -s /usr/src/linux-headers-3.10.17-yocto-standard /lib/modules/3.10.17-yocto-standard/build
 git clone https://git.open-mesh.org/batman-adv.git /home/root/batman-adv
 make --directory /home/root/batman-adv
 make --directory /home/root/batman-adv install 
